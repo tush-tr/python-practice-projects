8f24f6b93350c590cc5eeefe667f9e1544fbce42
tush-tr <iamtush-tr@DESKTOP-T3039R4.localdomain>
2021-01-08 12:15:50 +0530

    a health management system basic project

--- health-management-system/ex5.py
+++ health-management-system/ex5.py
@@ -6,15 +6,27 @@ def take(k):
         c=int(input("enter 1 for excersise and 2 for food"))
         if(c==1):
             value=input("type here\n")
-            with open("harry-ex.txt","a") as op:
+            with open("rahul-ex.txt","a") as op:
                 op.write(str([str(gettime())])+": "+value+"\n")
             print("successfully written")
         elif(c==2):
             value = input("type here\n")
-            with open("harry-food.txt", "a") as op:
+            with open("rahul-food.txt", "a") as op:
                 op.write(str([str(gettime())]) + ": " + value + "\n")
             print("successfully written")
     elif(k==2):
+        c = int(input("enter 1 for excersise and 2 for food"))
+        if (c == 1):
+            value = input("type here\n")
+            with open("sahul-ex.txt", "a") as op:
+                op.write(str([str(gettime())]) + ": " + value + "\n")
+            print("successfully written")
+        elif (c == 2):
+            value = input("type here\n")
+            with open("sahul-food.txt", "a") as op:
+                op.write(str([str(gettime())]) + ": " + value + "\n")
+            print("successfully written")
+    elif(k==3):
         c = int(input("enter 1 for excersise and 2 for food"))
         if (c == 1):
             value = input("type here\n")
@@ -26,32 +38,30 @@ def take(k):
             with open("rohan-food.txt", "a") as op:
                 op.write(str([str(gettime())]) + ": " + value + "\n")
             print("successfully written")
-    elif(k==3):
-        c = int(input("enter 1 for excersise and 2 for food"))
-        if (c == 1):
-            value = input("type here\n")
-            with open("hammad-ex.txt", "a") as op:
-                op.write(str([str(gettime())]) + ": " + value + "\n")
-            print("successfully written")
-        elif (c == 2):
-            value = input("type here\n")
-            with open("hammad-food.txt", "a") as op:
-                op.write(str([str(gettime())]) + ": " + value + "\n")
-            print("successfully written")
     else:
-        print("plz enter valid input (1(harry),2(rohan),3(hammad)")
+        print("plz enter valid input (1(Rahul),2(Sahul),3(Rohan)")
 def retrieve(k):
     if k==1:
         c=int(input("enter 1 for excersise and 2 for food"))
         if(c==1):
-            with open("harry-ex.txt") as op:
+            with open("rahul-ex.txt") as op:
                 for i in op:
                     print(i,end="")
         elif(c==2):
-            with open("harry-food.txt") as op:
+            with open("rahul-food.txt") as op:
                 for i in op:
                     print(i, end="")
     elif(k==2):
+        c = int(input("enter 1 for excersise and 2 for food"))
+        if (c == 1):
+            with open("sahul-ex.txt") as op:
+                for i in op:
+                    print(i, end="")
+        elif (c == 2):
+            with open("sahul-food.txt") as op:
+                for i in op:
+                    print(i, end="")
+    elif(k==3):
         c = int(input("enter 1 for excersise and 2 for food"))
         if (c == 1):
             with open("rohan-ex.txt") as op:
@@ -61,24 +71,17 @@ def retrieve(k):
             with open("rohan-food.txt") as op:
                 for i in op:
                     print(i, end="")
-    elif(k==3):
-        c = int(input("enter 1 for excersise and 2 for food"))
-        if (c == 1):
-            with open("hammad-ex.txt") as op:
-                for i in op:
-                    print(i, end="")
-        elif (c == 2):
-            with open("hammad-food.txt") as op:
-                for i in op:
-                    print(i, end="")
     else:
-        print("plz enter valid input (harry,rohan,hammad)")
-print("health management system: ")
-a=int(input("press 1 for lock the value and 2 for retrieve "))
+        print("plz enter valid input (Rahul,Sahul,Rohan)")
 
-if a==1:
-    b = int(input("press 1 for harry 2 for rohan 3 for hammad "))
+	
+
+print("-------health management system---------")
+a=int(input("press 1 for lock the value and 2 for retrieve "))
+b = int(input("press 1 for Rahul 2 for Sahul 3 for Rohan "))
+if(a==1):
+        # b = int(input("press 1 for Rahul 2 for Sahul 3 for Rohan "))
     take(b)
 else:
-    b = int(input("press 1 for harry 2 for rohan 3 for hammad "))
+        # b = int(input("press 1 for Rahul 2 for Sahul 3 for Rohan "))
     retrieve(b)
